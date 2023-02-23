d5106e1c4d68757be2857086ee8be501ae7c12bc
Ruben Alderete <Ruben.Alderete@Solera.com>
2023-02-23 15:42:01 +0100

    first commit of useEffect example code

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+const SimpleForm = () => {
+  const [formState, setformState] = useState({
+    username: "ruben",
+    email: "ruben@correo.com",
+  });
+
+  const { username, email } = formState;
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setformState({ ...formState, [name]: value });
+  };
+
+  useEffect(() => {
+    console.log("useEffect called!!");
+  });
+
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col text-center">
+            <h1>Simple Form</h1>
+
+            <input
+              type="text"
+              className="form-control mt-4"
+              placeholder="Username"
+              name="username"
+              value={username}
+              onChange={onInputChange}
+            />
+
+            <input
+              type="email"
+              className="form-control mt-3"
+              placeholder="email"
+              name="email"
+              value={email}
+              onChange={onInputChange}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SimpleForm;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import "./index.css";
 // import HooksApp from "./HooksApp";
 import CounterApp from "./01-useState/CounterApp";
 import CounterWithCustomHook from "./01-useState/CounterWithCustomHook";
+import SimpleForm from "./02-useEffect/SimpleForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <CounterWithCustomHook></CounterWithCustomHook>
-  </React.StrictMode>
+  //  <React.StrictMode>
+  <SimpleForm></SimpleForm>
+  // </React.StrictMode>
 );
