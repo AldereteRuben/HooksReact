e2cf1507865f005f18e21c841e7f9a118a861e79
Ruben Alderete <ruben.alderete@solera.com>
2023-03-21 14:49:47 +0100

    add a new example using useCallBack

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ import FocusScreen from "./04-useRefe/FocusScreen";
 import Layout from "./05-useLayoutEffect/Layout";
 import Memorize from "./06-memos/Memorize";
 import { CallbackHook } from "./06-memos/CallbackHook";
+import { Padre } from "./07-tarea-memo/Padre";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //  <React.StrictMode>
-  <CallbackHook />
+  <Padre />
   // </React.StrictMode>
 );
